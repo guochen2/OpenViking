3a7a31bedae55b91642588c7cfedf8ccd40d8d0e
chenguo <c.cg@foxmail.com>
2026-06-01 11:59:13 +0800

    add default web

---
+++ bot/user-web-studio/miaosalesweb/src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Miao Sales Web </q-toolbar-title>
+
+        <div>{{ version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+const version = process.env.APP_VERSION;
+</script>
---
+++ bot/user-web-studio/miaosalesweb/src/pages/IndexPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <q-page class="row items-center justify-evenly"> </q-page>
+</template>
+
+<script setup lang="ts">
+</script>
